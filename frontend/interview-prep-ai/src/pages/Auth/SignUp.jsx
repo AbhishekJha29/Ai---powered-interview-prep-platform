8c0407a85344819a0e6e2174b9def7ebf715d299
AbhishekJha29 <abbishekjha2286@gmail.com>
2025-11-18 18:28:03 +0530

    Made upto login page and the functioning of landingpage

---
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import { validateEmail } from '../../utils/helper';
+
+const SignUp = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl="";
+
+    if(!fullName){
+      setError("Please Enter Full Name")
+      return;
+    }
+
+
+    if (!validateEmail(email)) {
+      setError("please enter a valid email address.")
+      return;
+    }
+
+    if (!password) {
+      setError("please enter the password.")
+      return
+    }
+
+    setError("");
+
+    try {
+      
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message)
+      }else{
+        setError("Something went Wrong. Please Try again,")
+      }
+    }
+
+  }
+
+  return <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+    <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
+    <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below.</p>
+    <form onSubmit={handleSignUp}>
+
+      <ProfilePhotoSelector
+        image={profilePic}
+        setImage={setProfilePic}
+      />
+
+      <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
+        <Input value={fullName} onChange={({ target }) => setFullName(target.value)} label="Full Name" placeholder="John" type="text" />
+
+        <Input value={email} onChange={({ target }) => setEmail(target.value)} label="Email-Address" placeholder="john@example.com" type="text" />
+
+        <Input value={password} onChange={({ target }) => setPassword(target.value)} label="Email-password" placeholder="Min 8 Characters" type="password" />
+
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary'>SignUp</button>
+        <p className='text-[13px] text-slate-800 mt-3'>Already an Account?{""}
+          <button className='font-medium text-primary underline cursor-pointer' type='button' onClick={() => {
+            setCurrentPage("login")
+          }}
+          >
+            login
+          </button>
+        </p>
+
+      </div>
+    </form>
+
+  </div>
+
+}
+
+export default SignUp
